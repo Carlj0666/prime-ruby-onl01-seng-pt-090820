c782fa424e8eeda960974c90219ec38ec70c4c97
Carl J <carlj1881@yahoo.com>
2020-09-17 18:51:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,3 @@
-def prime?(num)     
-  number_array = []     
-    i=2     
-    while i < num         
-    number_array << i         
-    i += 1              end     number_array.none? { |dividend| num % dividend == 0} && num != 1 && num > 0 end
+def prime?(num)
+  
+end
