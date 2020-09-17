8d9579b61ca24804f63c0c803fc712295b632593
Carl J <carlj1881@yahoo.com>
2020-09-17 18:58:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,5 +2,5 @@ def prime?(num)
   number_array <<
   i += 1
 end
-  number_array.none?{ |dividend| num % dividend == 0} 
+  number_array.none?{ |dividend| num % dividend == 0} && num !=1 && num > 0
 end
